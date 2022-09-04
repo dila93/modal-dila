b52c54ee2ecbe905b16311154c286717ad40f727
Sebastian Ardla <sardila@veritran.com>
2022-09-04 12:32:15 -0500

    demo updated

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         <div>
-            Demooo
+            Demooo 2
             <Modal/>
         </div>
     </React.StrictMode>,
