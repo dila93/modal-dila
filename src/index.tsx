c703234f4dff219b9b7104803a2848184afb7888
Sebastian Ardla <sardila@veritran.com>
2022-09-04 12:24:24 -0500

    Initial paget commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Modal } from 'modal-dila';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+    <React.StrictMode>
+        <div>
+            Demooo
+            <Modal/>
+        </div>
+    </React.StrictMode>,
+);  
